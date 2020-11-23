11a645e42c683060290e2041a9661d579f0c1a2b
Alexander Romberg <adze@gmx.ch>
2020-11-23 16:19:23 +0100

    added favicon

--- src/views/experiment.php
+++ src/views/experiment.php
@@ -9,7 +9,20 @@ Copyright (c) 2020 Alexander Romberg, Dario Romandini
     <!-- stylesheets -->
     <link rel="stylesheet" href="../css/general.css">
     <link rel="stylesheet" href="../css/menu.css">
-    <link rel="stylesheet" href="../css/experiment.css">>
+    <link rel="stylesheet" href="../css/experiment.css">
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/src/res/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/src/res/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/src/res/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/src/res/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/src/res/favicon/safari-pinned-tab.svg" color="#0002ff">
+    <link rel="shortcut icon" href="/src/res/favicon/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="AtomAni">
+    <meta name="application-name" content="AtomAni">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="/src/res/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- scripts -->
     <script type="module" src="../js/simulation.js" defer></script>
