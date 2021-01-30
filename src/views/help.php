a429608479e2eb0cff1905c44a2ecc765ddf2aeb
Alexander Romberg <adze@gmx.ch>
2021-01-30 20:29:27 +0100

    updated help and footer

--- src/views/help.php
+++ src/views/help.php
@@ -35,47 +35,33 @@ Copyright (c) 2021 Alexander Romberg
             </nav>
             <main>
                 <img id="AtomAni" class="center" src="../res/logo.svg">
-                <p>AtomAni ist ein Programm, um Atome von Edelgasen zu simulieren.</p><p>Es ist im Rahmen einer Projektarbeit an der Informatikmittelschule Frauenfeld entstanden.</p><p>Der Name leitet sich aus den Wörtern Atom und Animation ab.</p><p>Als Grundlage für die Entwicklung diente das 2008 erschienene Programm Atomarium.</p>
+                <p>AtomAni ist ein Programm, um die Atombewegung von Edelgasen zu simulieren.</p><p>Es ist im Rahmen einer Projektarbeit an der Informatikmittelschule Frauenfeld entstanden.</p><p>Der Name leitet sich aus den Wörtern Atom und Animation ab.</p><p>Als Grundlage für die Entwicklung diente das 2008 erschienene Programm Atomarium.</p>
                 <h1 id="Selection">Experimentwahl</h1>
-                In der Experimentwahl lassen sich Experimente auswählen. Sie können gruppiert werden in Ordner, um einen besseren Überblick zu bekommen. Tippen Sie auf eine der Kacheln, um sie zu öffnen.
+                In der Experimentwahl werden die verfügbaren Experimente angezeigt. Sie können in Ordner gruppiert werden, um einen besseren Überblick zu bekommen. Tippen Sie auf eine der Kacheln, um sie zu öffnen.
                 <h1 id="Simulation">Simulation</h1>
-                Die Simulation besteht aus zwei Teilen, dem Simulationsfeld und dem Informationsfeld.
-                <h2>Simulationsfeld</h2>
-                In diesem Bereich werden die Atome dargestellt. Die Kamera der Simulation lässt sich frei bewegen, somit können die Atome von allen Seiten betrachten.
+                Die Simulation besteht aus zwei Bereichen, dem Simulationsbereich und dem Informationsbereich.
+                <h2>Simulationsbereich</h2>
+                In diesem Bereich werden die Atome als bewegte Kugeln dargestellt. Der Kameraausschnitt der Simulation lässt sich frei definieren, somit können die Atome von allen Seiten betrachtet werden.
                 <ul>
-                    <li>
-                        Durch Klicken/Tippen und Ziehen lässt sich die Kamera drehen.
-                    </li>
-                    <li>
-                        Durch Scrollen oder Wischen mit zwei Fingern lässt sich die Simulation vergrössern.
-                    </li>
-                    <li>
-                        Durch Ziehen mit Rechtsklick oder wischen mit zwei Fingern lässt sich die Kamera verschieben.
-                    </li>
+                    <li>Durch Klicken/Tippen und Ziehen lässt sich die Kamera drehen.</li>
+                    <li>Durch Scrollen oder Auseinanderziehen mit zwei Fingern lässt sich die Simulation vergrössern.</li>
+                    <li>Durch Ziehen mit Rechtsklick oder Wischen mit zwei Fingern lässt sich die Kamera verschieben.</li>
                 </ul>
-                <h2>Informationsfeld</h2>
-                <p>Hier werden die Diagramme und Steuerelemente angezeigt. Diagramme dienen der Visualisierung der Daten und werden in der Simulationskonfiguration aktiviert. Durch Steuerelemente lässt sich die Animation kontrollieren.</p>
+                <h2>Informationsbereich</h2>
+                <p>Hier werden die Diagramme und Steuerelemente angezeigt. Diagramme dienen der Visualisierung der Daten. Sie werden in der Simulationskonfiguration aktiviert. Durch Steuerelemente lässt sich die Animation kontrollieren.</p>
                 <p>Es stehen zurzeit folgende Diagramme und Steuerelemente zur Verfügung:</p>
                 <h3>Diagramme</h3>
                 <ul>
-                    <li>
-                        Bilder pro Sekunde [fps]
-                    </li>
-                    <li>
-                        Durchschnittliche Atomgeschwindigkeit [avgVel]
-                    </li>
+                    <li>Bilder pro Sekunde [fps]</li>
+                    <li>Durchschnittliche Atomgeschwindigkeit [avgVel]</li>
                 </ul>
                 <h3>Steuerelemente</h3>
                 <ul>
-                    <li>
-                        Kühlen/Heizen (Schieberegler) [temp]
-                    </li>
-                    <li>
-                        Simulationsbedienung (Simulationsgeschwindigkeit, Starten/Stoppen, Zurücksetzen) [control]
-                    </li>
+                    <li>Kühlen/Heizen (Schieberegler) [temp]</li>
+                    <li>Simulationsbedienung (Simulationsgeschwindigkeit, Starten/Stoppen, Zurücksetzen) [control]</li>
                 </ul>
                 <h1 id="Script">Simulationsskript</h1>
-                Eine Simulation wird durch eine <a href="https://www.json.org/json-de.html">JSON</a> Datei beschreiben. Diese muss aus mindestens vier Attributen bestehen.
+                Eine Simulation wird durch eine <a href="https://www.json.org/json-de.html">JSON</a> Datei beschrieben. Diese muss aus mindestens vier Attributen bestehen.
                 <h3>Beispiel</h3>
                 <code>{
     "charts": [],
@@ -83,8 +69,8 @@ Copyright (c) 2021 Alexander Romberg
     "walls": [],
     "controls": []
 }</code>
-                <h2>Diagramme</h2>
-                <p>Diagramme lassen sich mit Hilfe von IDs anzeigen. Diese sind im Bereich Informationsfeld in eckigen Klammern notiert.</p>
+                <h2>Diagramme [charts]</h2>
+                <p>Diagramme lassen sich mit Hilfe von IDs anzeigen. Diese sind im Abschnitt Informationsbereich in eckigen Klammern notiert.</p>
                 <p>Ein Diagramm besteht aus folgenden Informationen:</p>
                 <table>
                     <tr>
@@ -97,7 +83,7 @@ Copyright (c) 2021 Alexander Romberg
                             id
                         </td>
                         <td>
-                            Kürzel, das die Art des Diagramms bestimmt.
+                            Kürzel, das die Art des Diagramms bestimmt
                         </td>
                         <td>
                             "avgVel"
@@ -108,7 +94,7 @@ Copyright (c) 2021 Alexander Romberg
                             title
                         </td>
                         <td>
-                            Text der über dem Diagramm steht.
+                            Text, der über dem Diagramm steht
                         </td>
                         <td>
                             "Geschwindigkeit"
@@ -119,7 +105,7 @@ Copyright (c) 2021 Alexander Romberg
                             lineColor
                         </td>
                         <td>
-                            Linienfarbe des Diagramms als rgba-Wert
+                            Linienfarbe des Diagramms
                         </td>
                         <td>
                             "rgba(0,0,200,1)"
@@ -144,9 +130,9 @@ Copyright (c) 2021 Alexander Romberg
     "lineColor": "rgba(0,0,200,1)",
     "fillColor": "rgba(0,0,170,0.4)"
 }]</code>
-                <h2>Atome</h2>
+                <h2>Atome [atoms]</h2>
                 <p>Atome könne einzeln oder als Gitter platziert werden und haben verschiedene Atomtypen (Farben).</p>
-                <p>Ein Atom oder eine Atom-Struktur besteht aus folgenden Informationen:</p>
+                <p>Ein Atom oder eine Atom-Struktur (Gitter) besteht aus folgenden Informationen:</p>
                 <table>
                     <tr>
                         <th>Name</th>
@@ -180,7 +166,7 @@ Copyright (c) 2021 Alexander Romberg
                             width, height, depth
                         </td>
                         <td>
-                            Anzahl Atome im Gitter, in der jeweiligen Richtung. <i>Nur bei type:grid</i>.
+                            Anzahl Atome im Gitter, in der jeweiligen Richtung<br><i>Nur bei type:grid</i>.
                         </td>
                         <td>
                             5
@@ -216,9 +202,9 @@ Copyright (c) 2021 Alexander Romberg
     "z": 240,
     "atomType": "ar"
 }]</code>
-                <h2>Wände</h2>
+                <h2>Wände [walls]</h2>
                 <p>Wände können in Form von Boxen erstellt werden.</p>
-                <p>Eine Wand besteht aus folgenden Informationen:</p>
+                <p>Eine Box besteht aus folgenden Informationen:</p>
                 <table>
                     <tr>
                         <th>Name</th>
@@ -257,8 +243,8 @@ Copyright (c) 2021 Alexander Romberg
     "height": 600,
     "depth": 600
 }]</code>
-                <h2>Kontrollelemente</h2>
-                Kontrollelemente lassen sich mithilfe von IDs auswählen
+                <h2>Kontrollelemente [controls]</h2>
+                Kontrollelemente lassen sich mithilfe von IDs auswählen.
                 Ein Kontrollelement besteht aus folgenden Informationen:
                 <table>
                     <tr>
