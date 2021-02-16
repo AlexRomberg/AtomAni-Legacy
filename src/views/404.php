7889795d3ff04dc4afb2a299e051a789e92e5703
Alexander Romberg <adze@gmx.ch>
2021-02-16 20:59:14 +0100

    added 404 Page (Closes #4)

---
+++ src/views/404.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+Copyright (c) 2021 Alexander Romberg
+-->
+
+<head>
+    <title>AtomAni</title>
+
+    <?php require('favicon.php') ?>
+
+</head>
+
+<body>
+    <style>
+        <?php
+        echo file_get_contents("../css/general.css");
+        echo file_get_contents("../css/404.css");
+        ?>
+    </style>
+
+    <div class="container">
+        <?php
+        $title = "404 - not found";
+        require('header.php')
+        ?>
+        <main>
+            <h1>Die Webseite ist nicht Erreichbar.</h1>
+            <svg width="100%" height="100%" viewBox="0 0 1920 1080" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                <g id="Artboard1" transform="matrix(0.96,0,0,0.54,0,0)">
+                    <rect x="0" y="0" width="2000" height="2000" style="fill:none;" />
+                    <g transform="matrix(0.655177,0,0,1.16476,344.823,-278.734)">
+                        <g id="M" transform="matrix(1.28044,0,0,1.28044,-432.622,-6609.87)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="D" transform="matrix(1.28044,0,0,1.28044,-432.622,-6484.51)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="U" transform="matrix(1.28044,0,0,1.28044,-432.622,-6739.1)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="L" transform="matrix(1.28044,0,0,1.28044,-557.475,-6609.87)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="DL" transform="matrix(1.28044,0,0,1.28044,-557.475,-6484.51)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="UL" transform="matrix(1.28044,0,0,1.28044,-557.475,-6739.1)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="R" transform="matrix(1.28044,0,0,1.28044,-307.769,-6609.87)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="DR" transform="matrix(1.28044,0,0,1.28044,-307.769,-6484.51)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="UR" transform="matrix(1.28044,0,0,1.28044,-307.769,-6739.1)">
+                            <circle cx="1118.85" cy="6021.11" r="38.665" style="fill:rgb(0,2,255);" />
+                        </g>
+                        <g id="MDL" transform="matrix(1,0,1.11022e-16,1,-2479.11,-664.457)">
+                            <circle cx="3420.3" cy="1821.05" r="27.503" style="fill:rgb(0,131,255);" />
+                        </g>
+                        <g id="MDR" transform="matrix(1,0,1.11022e-16,1,-2361.49,-664.457)">
+                            <circle cx="3420.3" cy="1821.05" r="27.503" style="fill:rgb(0,131,255);" />
+                        </g>
+                        <g id="MUR" transform="matrix(1,0,1.11022e-16,1,-2361.49,-782.078)">
+                            <circle cx="3420.3" cy="1821.05" r="27.503" style="fill:rgb(0,131,255);" />
+                        </g>
+                        <g id="MUL" transform="matrix(1,0,1.11022e-16,1,-2479.11,-782.078)">
+                            <circle cx="3420.3" cy="1821.05" r="27.503" style="fill:rgb(0,131,255);" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+        </main>
+        <?php require("footer.php") ?>
+    </div>
+    <script>
+        setTimeout(() => {
+            document.querySelector("main svg").classList.add("animate");
+        }, 500);
+    </script>
+</body>
