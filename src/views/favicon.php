c59910a0fc77bfbab137e08ff89f9308a929a5cd
Alexander Romberg <adze@gmx.ch>
2021-02-23 10:36:17 +0100

    updated favicon

--- src/views/favicon.php
+++ src/views/favicon.php
@@ -2,10 +2,11 @@
 <link rel="icon" type="image/png" sizes="32x32" href="../res/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../res/favicon/favicon-16x16.png">
 <link rel="manifest" href="../res/favicon/site.webmanifest">
-<link rel="mask-icon" href="../res/favicon/safari-pinned-tab.svg" color="#0002ff">
+<link rel="mask-icon" href="../res/favicon/safari-pinned-tab.svg" color="#0066ff">
 <link rel="shortcut icon" href="../res/favicon/favicon.ico">
 <meta name="apple-mobile-web-app-title" content="AtomAni">
 <meta name="application-name" content="AtomAni">
-<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileColor" content="#0066ff">
+<meta name="msapplication-TileImage" content="../res/favicon/mstile-144x144.png">
 <meta name="msapplication-config" content="../res/favicon/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
